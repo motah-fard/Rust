7b0ced87a016612b902f8a3fd0ccf1dac019a505
ati-da <atefeh.daemi@gmail.com>
2025-05-20 12:33:23 -0700

    run

--- deckOfCards/src/main.rs
+++ deckOfCards/src/main.rs
@@ -1,9 +1,11 @@
+#[derive(Debug)]
+
 struct Deck {
-    cards: Vec<String>
+    cards: Vec<String>,
 }
 
 
 fn main() {
     let deck : Deck = Deck { cards: vec![] };
-    println!("Here is your deck: {}", deck);
+    println!("Here is your deck: {:?}", deck);
 }
