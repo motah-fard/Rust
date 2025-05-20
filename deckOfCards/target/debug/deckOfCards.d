55d00dcd509aa1dbbf939bd14563cf30f367b3c4
ati-da <atefeh.daemi@gmail.com>
2025-05-20 12:09:40 -0700

    run

---
+++ deckOfCards/target/debug/deckOfCards.d
@@ -0,0 +1,1 @@
+/Users/motaharehshafieifard/Documents/git/Rust/Project/deckOfCards/target/debug/deckOfCards: /Users/motaharehshafieifard/Documents/git/Rust/Project/deckOfCards/src/main.rs
---
+++ deckOfCards/target/debug/deps/deckOfCards-091a920039000d79.d
@@ -0,0 +1,5 @@
+/Users/motaharehshafieifard/Documents/git/Rust/Project/deckOfCards/target/debug/deps/deckOfCards-091a920039000d79: src/main.rs
+
+/Users/motaharehshafieifard/Documents/git/Rust/Project/deckOfCards/target/debug/deps/deckOfCards-091a920039000d79.d: src/main.rs
+
+src/main.rs:
