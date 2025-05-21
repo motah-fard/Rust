b8b547705fb6e77304675dfe022530781ffb2599
motah-fard <motah.fard@gmail.com>
2025-05-21 13:35:55 -0700

    run

--- deckOfCards/src/main.rs
+++ deckOfCards/src/main.rs
@@ -5,20 +5,31 @@ struct Deck {
 }
 
 
-fn main() {
-    // list of 'suits' - 'hearts', 'spades', ..
-    let suits = vec!["Hearts", "Spades", "Diamonds", "Clubs"];
-    // list of 'values' - 'ace', 'two', 'three', ..
-    let values = vec!["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+impl Deck {
+    fn new() -> Self {
+        // list of 'suits' - 'hearts', 'spades', ..
+        let suits = vec!["Hearts", "Spades", "Diamonds", "Clubs"];
+        // list of 'values' - 'ace', 'two', 'three', ..
+        let values = vec!["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
 
-    let mut cards = vec![];
-    // double nested for loop 
-    for suit in suits { 
-        for value in &values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+
+        let mut cards = vec![];
+        
+        // double nested for loop 
+        for suit in suits {
+            for value in &values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+
+        let deck = Deck { cards };
+        return deck;
     }
-    let deck = Deck { cards: cards };
-    println!("Here is your deck: {:#?}", deck);
+}
+
+fn main() {
+    let deck = Deck::new();
+
+    println!("Heres your deck: {:#?}", deck);
 }
