c70fad9b5a8c2382acf021f56edd92106bbd7cbc
motah-fard <motah.fard@gmail.com>
2025-05-20 18:44:20 -0700

    run

--- deckOfCards/src/main.rs
+++ deckOfCards/src/main.rs
@@ -6,6 +6,19 @@ struct Deck {
 
 
 fn main() {
+    // list of 'suits' - 'hearts', 'spades', ..
+    let suits = vec!["Hearts", "Spades", "Diamonds", "Clubs"];
+    // list of 'values' - 'ace', 'two', 'three', ..
+    let values = vec!["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+
+    let cards = vec![];
+    // double nested for loop 
+    for suit in suits { 
+        for value in values {
+            let card = format!("{} of {}", value, suit)
+            cards.push(card);
+        }
+    }
     let deck : Deck = Deck { cards: vec![] };
     println!("Here is your deck: {:?}", deck);
 }
